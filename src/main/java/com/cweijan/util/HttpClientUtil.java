9b0b78c9573a6c701d1ec5e0927c44e45acf3e5a
cweijan <cweijan@163.com>
2018-03-15 15:07:05 +0800

    鈥榓dd鈥

---
+++ src/main/java/com/cweijan/util/HttpClientUtil.java
@@ -0,0 +1,206 @@
+package com.tcsoft.framework.utils;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
+import java.util.Map.Entry;
+
+import javax.net.ssl.SSLException;
+
+import org.springframework.beans.factory.annotation.Value;
+public final class HttpClientUtil {
+
+	/**
+	 * 执行一个HTTP POST请求，返回请求响应的HTML
+	 * @param url 		请求的URL地址
+	 * @param params 	请求的查询参数
+	 * @return 			返回请求响应
+	 */
+	@Value("${CONNECT_TIME_OUT}")
+	private  static int CONNECT_TIME_OUT; //连接超时时间
+	@Value("${READ_OUT_TIME}")
+    private  static int READ_OUT_TIME;  //读取超时时间
+    
+	public static String doPost(String url, Map<String, String> params,boolean[] connectFlag) {
+		URL u = null;
+		HttpURLConnection con = null;
+		// 构建请求参数
+		StringBuffer sb = new StringBuffer();
+		if (params != null) {
+			for (Entry<String, String> e : params.entrySet()) {
+				sb.append(e.getKey());
+				sb.append("=");
+				sb.append(e.getValue());
+				sb.append("&");
+			}
+			sb.substring(0, sb.length() - 1);
+		}
+		System.out.println("send_url:" + url);
+		System.out.println(sb.length());
+		//System.out.println("send_data:" + sb.toString());
+		// 读取返回内容
+		StringBuffer buffer = new StringBuffer();
+		// 尝试发送请求
+		try {
+			u = new URL(url);
+			con = (HttpURLConnection) u.openConnection();
+			con.setRequestMethod("POST");
+			con.setDoOutput(true);
+			con.setDoInput(true);
+			con.setUseCaches(false);
+			con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			OutputStreamWriter osw = new OutputStreamWriter( con.getOutputStream(), "UTF-8");
+			osw.write(sb.toString());
+			osw.flush();
+			osw.close();
+		} catch (Exception e) {
+			buffer.append("访问服务器出错！");
+			e.printStackTrace();
+		} finally {
+			if (con != null) {
+				con.disconnect();
+			}
+		}
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					con.getInputStream(), "UTF-8"));
+			String temp;
+			while ((temp = br.readLine()) != null) {
+				buffer.append(temp);
+				//buffer.append("\n");
+			}
+		} catch (Exception e) {
+			buffer.append("读取返回结果出错！");
+			e.printStackTrace();
+		}
+		connectFlag[0] = true;
+		return buffer.toString();
+	}
+	//生成一个随机生成的字符串
+	private static String boundaryString = getBoundary();
+	/**
+	 * 发送人脸识别请求
+	 * @return
+	 */
+	public static byte[] sendFacePost(String url, HashMap<String, String> map, HashMap<String, byte[]> fileMap) throws Exception {
+        HttpURLConnection conne;
+        URL url1 = new URL(url);
+        conne = (HttpURLConnection) url1.openConnection();
+        conne.setDoOutput(true);
+        conne.setUseCaches(false);
+        conne.setRequestMethod("POST");
+        conne.setConnectTimeout(CONNECT_TIME_OUT);
+        conne.setReadTimeout(READ_OUT_TIME);
+        conne.setRequestProperty("accept", "*/*");
+        conne.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;boundary=" + boundaryString);
+        //conne.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundaryString);
+        conne.setRequestProperty("connection", "Keep-Alive");
+        conne.setRequestProperty("user-agent", "Mozilla/4.0 (compatible;MSIE 6.0;Windows NT 5.1;SV1)");
+        DataOutputStream obos = new DataOutputStream(conne.getOutputStream());
+        Iterator iter = map.entrySet().iterator();
+        while(iter.hasNext()){
+            Map.Entry<String, String> entry = (Map.Entry) iter.next();
+            String key = entry.getKey();
+            String value = entry.getValue();
+            obos.writeBytes("--" + boundaryString + "\r\n");
+            obos.writeBytes("Content-Disposition: form-data; name=\"" + key+ "\"\r\n");
+            obos.writeBytes("\r\n");
+            obos.writeBytes(value + "\r\n");
+        }
+        if(fileMap != null && fileMap.size() > 0){
+            Iterator fileIter = fileMap.entrySet().iterator();
+            while(fileIter.hasNext()){
+                Map.Entry<String, byte[]> fileEntry = (Map.Entry<String, byte[]>) fileIter.next();
+                obos.writeBytes("--" + boundaryString + "\r\n");
+                obos.writeBytes("Content-Disposition: form-data; name=\"" + fileEntry.getKey()
+                        + "\"; filename=\"" + encode(" ") + "\"\r\n");
+                obos.writeBytes("\r\n");
+                obos.write(fileEntry.getValue());
+                obos.writeBytes("\r\n");
+            }
+        }
+        obos.writeBytes("--" + boundaryString + "--" + "\r\n");
+        obos.writeBytes("\r\n");
+        obos.flush();
+        obos.close();
+        InputStream ins = null;
+        int code = conne.getResponseCode();
+        try{
+            if(code == 200){
+                ins = conne.getInputStream();
+            }else{
+                ins = conne.getErrorStream();
+            }
+        }catch (SSLException e){
+            e.printStackTrace();
+            return new byte[0];
+        }
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buff = new byte[4096];
+        int len;
+        while((len = ins.read(buff)) != -1){
+            baos.write(buff, 0, len);
+        }
+        byte[] bytes = baos.toByteArray();
+        ins.close();
+        return bytes;
+	}
+	 /**
+     * 生成一个随机字符串
+     * @return
+     */
+	private static String getBoundary() {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for(int i = 0; i < 32; ++i) {
+            sb.append("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-".charAt(random.nextInt("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_".length())));
+        }
+        return sb.toString();
+    }
+	/**
+     * 加密一个字符串
+     * @param value
+     * @return
+     * @throws Exception
+     */
+    private static String encode(String value) throws Exception{
+        return URLEncoder.encode(value, "UTF-8");
+    }
+    
+    /**
+     * 从一个文件获取字节数组
+     * @param f
+     * @return
+     */
+    public static byte[] getBytesFromFile(File f) {
+        if (f == null) {
+            return null;
+        }
+        try {
+            FileInputStream stream = new FileInputStream(f);
+            ByteArrayOutputStream out = new ByteArrayOutputStream(1000);
+            byte[] b = new byte[1000];
+            int n;
+            while ((n = stream.read(b)) != -1)
+                out.write(b, 0, n);
+            stream.close();
+            out.close();
+            return out.toByteArray();
+        } catch (IOException e) {
+        }
+        return null;
+    }
+}
